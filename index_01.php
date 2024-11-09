e3b80e49baebbc9b1514b7da7dcea594b2a2705a
Muhammad Kashif <139557529+Kashif5103@users.noreply.github.com>
2024-11-09 13:58:09 +0500

    Intro of the PHP

---
+++ index_01.php
@@ -0,0 +1,22 @@
+<?php
+// Single line comment 
+echo "Hi kashif i am developer";
+/*this is the multi line 
+comment*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Project</title>
+</head>
+<body>
+    <h1><?php echo"i Am a PHP Developer"  ?></h1>    
+<?php 
+echo ""
+?>
+
+</body>
+</html>
