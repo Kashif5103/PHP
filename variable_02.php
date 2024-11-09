0413f1061d18bf47f1d3e578da9de80829a93857
Muhammad Kashif <139557529+Kashif5103@users.noreply.github.com>
2024-11-09 13:59:03 +0500

    variables in the php

---
+++ variable_02.php
@@ -0,0 +1,36 @@
+<?php
+
+// Variable
+// A reudeable container that holdes the data
+// Data types
+// String ,float ,integer boolean
+
+
+
+$name ="Kashif";
+$age=23;
+$marks=75.12;
+$isMarried=false;
+
+echo "MY name is ".$name;
+echo "<br>MY age is ".$age;
+echo "<br>MY Marks is ".$marks;
+echo "<br>Are You married are not ".$marks;
+
+var_dump($name);
+echo "<br>";
+var_dump($age);
+echo "<br>";
+var_dump($marks);
+echo "<br>";
+var_dump($isMarried)
+
+// Variables Rules
+// A variable starts with the $ sign, followed by the name of the variable
+// A variable name must start with a letter or the underscore character
+// A variable name cannot start with a number
+// A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+// Variable names are case-sensitive ($age and $AGE are two different variables)
+
+
+?>
