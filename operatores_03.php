70ec5d0bd95969ddb98acf1e7aafb2b7f2c09842
Muhammad Kashif <139557529+Kashif5103@users.noreply.github.com>
2024-11-09 23:08:18 +0500

    Add the Operator logic

---
+++ operatores_03.php
@@ -0,0 +1,46 @@
+<?php
+// Operators 
+// Arthmatic operator
+// +-/* **%
+// Increment / Decrement opetators 
+// ++ --
+// Operator Predence
+// []
+// **
+// */+
+// +-
+// 
+
+$a=10;
+$b=3;
+// $c=$a+$b;
+// echo $c;
+// $c=$a-$b;
+// echo $c;
+// $c=$a/$b;
+// echo $c;
+// $c=$a*$b;
+// echo $c;
+// $c=$a%$b;
+// echo $c;
+// $c=$a**$b;
+// echo $c;
+// increment and decrement
+// echo $a;
+// $a++;
+// echo $a;
+// $a--;
+// echo $a;
+// ++$a;
+// echo $a;
+// --$a;
+// echo $a;
+
+$total=4/2*3+(5+10)**2;
+// 4/2*3+(15)**2
+// 4/2*3+225
+// 2*3+225
+// 6+225
+// 231
+echo $total;
+?>
