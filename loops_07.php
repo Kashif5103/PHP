3032f9335cfc2bc3ae16548d0b12fdc5a661c193
Muhammad Kashif <139557529+Kashif5103@users.noreply.github.com>
2024-11-10 22:18:07 +0500

    details of the loops

---
+++ loops_07.php
@@ -0,0 +1,17 @@
+<?php
+// for loops
+// For loop is used for the where the last condion is given
+// repeat the some code for the certain period of time
+// for ($i=0;$i<5;$i++){
+//     echo "Hi kashif<br>";
+// }
+// for ($i=10;$i>=0;$i--){
+//     echo $i."<br>";
+// }
+// table 
+for ($i=1;$i<=10;$i++){
+    echo 2 ."X" .$i."=".$i*2;
+}
+
+
+?>
