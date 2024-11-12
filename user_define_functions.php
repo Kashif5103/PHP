aa6fda56ef6fbe62cd7f35113869487a3eed68e3
Muhammad Kashif <139557529+Kashif5103@users.noreply.github.com>
2024-11-12 23:43:33 +0500

    details of the user define functions

---
+++ user_define_functions.php
@@ -0,0 +1,13 @@
+<?php
+// user define function 
+// a function is a block of code that can be reused in a program
+// 1: function define 
+// 2: function call
+
+function kashif($i,$J){
+    echo "Welcome from the Cat marketing".$i+$J;
+}
+
+kashif(12,45);
+
+?>
